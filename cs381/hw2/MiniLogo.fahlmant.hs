820c76e4a470da2329ba3064bfd47aa083343dd0
fahlmant <fahlmant@osuosl.org>
2017-01-30 09:16:49 -0800

    Hide num from prelude, add constructors to Expr, and fix line and nix to at least compile

--- cs381/hw2/MiniLogo.fahlmant.hs
+++ cs381/hw2/MiniLogo.fahlmant.hs
@@ -3,7 +3,7 @@
 
 module HW2 where
 
-import Prelude --hide Num
+import Prelude hiding (Num) --hide Num
 
 data Cmd = SetPen Mode
          | Move Expr Expr
@@ -13,7 +13,7 @@ data Cmd = SetPen Mode
 type Num  = Int
 type Var  = String
 type Macro = String
-data Expr = Var | Num | Expr  Expr
+data Expr = Variable Var | Number Num | Pair Expr  Expr
 data Mode = Up | Down
 type Prog = [Cmd]
 
@@ -23,15 +23,15 @@ type Prog = [Cmd]
 --   pen down;
 --   move (x2, y2);
 -- }
-line :: Int -> Int -> Int -> Int -> Prog
-line x1 y1 x2 y2= Define "line" [x1, y1, x2, y2] [SetPen Up, Move x1 y1,  SetPen Down, Move x2 y2]
+line :: Num -> Num -> Num -> Num -> Prog
+line x1 y1 x2 y2 = [Define "line" ["x1", "y1", "x2", "y2"] [SetPen Up, SetPen Down]]
 
 
 -- define nix (x, y, w, h) {
 --   line(x,   y, x+w, y+h);
 --   line(x+w, y, x,   y+h);
 -- }
-nix :: Int -> Int -> Int -> Int -> Prog
-nix x y w h = Define "nix" [x, y, w, h] [line(x, y, x+w, y+h), line(x+w, y, x, y+h)]
+nix :: Num -> Num -> Num -> Num -> Prog
+nix x y w h = [Define "nix" ["x", "y", "w", "h"] [line x y (x+w) (y+h), line (x+w) y x (y+h)]]
 
 
