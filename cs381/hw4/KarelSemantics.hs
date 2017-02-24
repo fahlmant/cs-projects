c302855253b0ec207f04b94d8fe5073a20324858
Taylor <fahlmantaylor@gmail.com>
2017-02-23 18:57:21 -0800

    pretty much got everything done but Call. Some of these are non exauhstive thought

--- cs381/hw4/KarelSemantics.hs
+++ cs381/hw4/KarelSemantics.hs
@@ -21,7 +21,7 @@ stmt Shutdown   _ _ r = Done r
 stmt Move       _ w (p, c, i) = let x = neighbor c p
                                 in if (isClear x w)
                                       then OK w (setPos x (p, c, i))
-                                      else Error ("Can't move there")
+                                      else Error ("Blocked at: " ++ show x)
 stmt PickBeeper _ w r = let p = getPos r
                         in if hasBeeper p w
                               then OK (decBeeper p w) (incBag r)
@@ -32,11 +32,20 @@ stmt PutBeeper  _ w r = let p = getPos r
                               else OK (incBeeper p w) (decBag r)
 stmt (Turn d)      _ w (p, c, i) = let r' = setFacing (cardTurn d c) (p, c, i)
                                    in OK w r'
-stmt (Call m)      _ _ _ = undefined
-stmt (Iterate i s) _ _ _ = undefined
-stmt (If t s1 s2 ) _ _ _ = undefined
-stmt (While t s  ) _ _ _ = undefined
-stmt (Block (x:xs)) _ _ _ = undefined
+stmt (Call m)      d w r = undefined 
+stmt (Iterate 0 _) _ w r = OK w r
+stmt (Iterate i s) d w r = case stmt s d w r of
+                           OK w' r' -> stmt (Iterate (i-1) s) d w' r'
+stmt (If t s1 s2 ) d w r = if test t w r
+                              then stmt s1 d w r
+                              else stmt s2 d w r
+stmt (While t s  )  d w r = if test t w r
+                               then case stmt s d w r of
+                                 OK w' r' -> stmt (While t s) d w' r'
+                            else OK w r
+stmt (Block []   )  _ w r = OK w r
+stmt (Block (x:xs)) d w r = case stmt x d w r of
+                            OK w' r' -> stmt (Block xs) d w' r'
 
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
