0ac97cd0c2c7a441ee4d126791f8f62db6fe19e2
Taylor <fahlmantaylor@gmail.com>
2017-02-27 07:34:09 -0800

    Finish hw4

--- cs381/hw4/KarelSemantics.hs
+++ cs381/hw4/KarelSemantics.hs
@@ -17,36 +17,40 @@ test (Empty   ) _  r        = isEmpty r
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
-stmt Shutdown   _ _ r = Done r
-stmt Move       _ w (p, c, i) = let x = neighbor c p
-                                in if (isClear x w)
-                                      then OK w (setPos x (p, c, i))
-                                      else Error ("Blocked at: " ++ show x)
-stmt PickBeeper _ w r = let p = getPos r
-                        in if hasBeeper p w
-                              then OK (decBeeper p w) (incBag r)
-                              else Error ("No beeper to pick at: " ++ show p)
-stmt PutBeeper  _ w r = let p = getPos r
-                        in if isEmpty r
-                              then Error ("No beeper to put")
-                              else OK (incBeeper p w) (decBag r)
+stmt Shutdown     _ _ r = Done r
+stmt Move         _ w (p, c, i) = let x = neighbor c p
+                                  in if (isClear x w)
+                                        then OK w (setPos x (p, c, i))
+                                        else Error ("Blocked at: " ++ show x)
+stmt PickBeeper   _ w r = let p = getPos r
+                          in if hasBeeper p w
+                                then OK (decBeeper p w) (incBag r)
+                                else Error ("No beeper to pick at: " ++ show p)
+stmt PutBeeper     _ w r = let p = getPos r
+                           in if isEmpty r
+                                 then Error ("No beeper to put.")
+                                 else OK (incBeeper p w) (decBag r)
 stmt (Turn d)      _ w (p, c, i) = let r' = setFacing (cardTurn d c) (p, c, i)
                                    in OK w r'
-stmt (Call m)      d w r = undefined 
+stmt (Call m)      d w r = case lookup m d of
+                             Just m' -> stmt m' d w r
+                             Nothing -> Error ("Undefined macro: " ++ m)
 stmt (Iterate 0 _) _ w r = OK w r
 stmt (Iterate i s) d w r = case stmt s d w r of
                            OK w' r' -> stmt (Iterate (i-1) s) d w' r'
+                           otherwise -> otherwise
 stmt (If t s1 s2 ) d w r = if test t w r
                               then stmt s1 d w r
                               else stmt s2 d w r
-stmt (While t s  )  d w r = if test t w r
+stmt (While t s  ) d w r = if test t w r
                                then case stmt s d w r of
                                  OK w' r' -> stmt (While t s) d w' r'
+                                 otherwise -> otherwise
                             else OK w r
 stmt (Block []   )  _ w r = OK w r
 stmt (Block (x:xs)) d w r = case stmt x d w r of
                             OK w' r' -> stmt (Block xs) d w' r'
-
+                            otherwise -> otherwise
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
 prog (m,s) w r = stmt s m w r
