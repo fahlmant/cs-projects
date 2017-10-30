182d03458d83c3048e6efd9fa8fd95627f5d955d
Taylor Fahlman <fahlmantaylor@gmail.com>
2017-10-29 23:51:53 -0700

    Fix up program

--- cs372/assignment1/chatclient.cpp
+++ cs372/assignment1/chatclient.cpp
@@ -1,16 +1,32 @@
-#include <stdio.h>
+/****
+ *Taylor Fahlman
+ * CS 372, Section 400
+ * Assignment 1
+ * Source: http://www.geeksforgeeks.org/socket-programming-cc/
+ * http://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/socket.html
+ * https://github.com/angrave/SystemProgramming/wiki/Networking,-Part-2:-Using-getaddrinfo
+ * https://stackoverflow.com/questions/7352099/stdstring-to-char
+ * */
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <sys/socket.h>
-#include <sys/types.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
-#include <iostream>
+#include <string.h>
 
+//Makes the socket to connect to the server
 int make_socket() {
 
+    //Declare variables
     int fd;
     int result;
 
+    //Create a TCP socket using the Sockets API and abort if the socket isn't created
     fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd == -1) {
         printf("Error making socket, aborting\n");
@@ -20,60 +36,78 @@ int make_socket() {
     return fd;
 }
 
+//Initializes connection from client to server
+void init_connection_socket(int fd, struct sockaddr_in *addr ) {
 
-void init_connection_socket(int fd, struct addrinfo *addr ) {
-
-    int result = connect(fd, addr->ai_addr, addr->ai_addrlen);
+    //Uses connect() call from sockets, passing the file descriptor,
+    //address and address length
+//    int result = connect(fd, addr->ai_addr, addr->ai_addrlen);
+    int result = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
     if(result == -1) {
         printf("Error connecting, aborting\n");
         exit(1);
     }
 }
 
+//Performs initial handshake of exchanging handles from client to server
 void connect_to_server(int fd, char* handle, char* serverhandle) {
 
-    
+    //send handle to the server via the socket
     int status = send(fd, handle, strlen(handle), 0);
+    //Recieve up to 10 characters from the server, which is the server handle
     recv(fd, serverhandle, 10, 0);
 }
 
+//Loop over sending and recieving messages from server
 void message_loop(int fd, char* handle, char* serverhandle) {
 
+    //Create buffers for messages, zero them out, and initialize other variables
     char message_to_send[500];
     char message_to_recv[500];
     int sent_bytes;
     int recv_bytes;
     memset(message_to_send, 0, sizeof(message_to_send));
     memset(message_to_recv, 0, sizeof(message_to_recv));
+
+    //Start loop
     while(1) {
+        //Prompt user for message to send
         printf("%s> ", handle);
         fgets(message_to_send, 501, stdin);
+        //If the user enters "\quit", gracefully close the program
         if(strcmp(message_to_send, "\\quit\n") == 0){
             exit(0);
         }
+        //Send message that user gave to the server over the file descriptor.
         sent_bytes = send(fd, message_to_send, strlen(message_to_send), 0);
         if(sent_bytes == -1) {
             printf("Error in communications with client\n");
             exit(1);
         }
+        //Wait and recieve message from server
         recv_bytes = recv(fd, message_to_recv, 501, 0);
+        //If the status of the recieving is -1, abort
         if(recv_bytes == -1){
             printf("Error in recieving communication\n");
             exit(1);
         }
+        //If the server sends nothing, the server must have \quit, so close
         else if(recv_bytes == 0) {
             printf("Connection terminated\n");
             exit(0);
         }
+        //Otherwise, print out whatever the server sent us.
         else {
             printf("%s> %s\n", serverhandle, message_to_recv);
         }
     
+        //Ensure that buffers are zeroed out before next loop
         memset(message_to_send, 0, sizeof(message_to_send));
         memset(message_to_recv, 0, sizeof(message_to_recv));
    }
 }
 
+//Prompt user for handle, and store it in a char* variable
 void get_handle(char *handle) {
     printf("Enter a handle: ");
     scanf("%s", handle);
@@ -94,12 +128,22 @@ int main(int argc, char** argv) {
 
     //Declare addrinfo structs, feed in both hostname and port. 
     struct addrinfo hints, *addr_info;
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(struct sockaddr_in));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(atoi(argv[2]));
+    inet_pton(server_addr.sin_family, argv[1], &server_addr.sin_addr);
 //    int result = getaddrinfo(argv[1], argv[2], &hints, &addr_info);
 
+    //Get te handle from the user
     get_handle(handle);
+    //Create socket
     fd = make_socket();
-    init_connection_socket(fd, addr_info);
+    //Connect socket to server
+    init_connection_socket(fd, &server_addr);
+    //Handshake, and exchange handles
     connect_to_server(fd, handle, serverhandle);
+    //Loop sending and recieving messages until user quits. 
     message_loop(fd, handle, serverhandle);
 
     return 0;
--- cs372/assignment1/chatserve.py
+++ cs372/assignment1/chatserve.py
@@ -2,35 +2,46 @@
 import socket
 import sys
 
+#Function prints missing port number error and exits if user doesn't provide port.
 def valid_port_error():
     print('Please provide a valid port number')
     sys.exit(0)
 
-
+#Connects to client by receiving the client's handle, then sending the server's handle.
 def connect_to_client(sock, handle):
     clienthandle = sock.recv(1024)
     sock.send(handle)
     return clienthandle
 
+#Loops repeatedly once the handshake between the client and server is done
+#and handles message sneding and recieving
 def message_loop(sock, clienthandle, handle):
     message_send = '' 
     while 1:
+        #Recieves up to 500 bytes from the client
         message_recv = sock.recv(500)
+        #If the client sends nothing, the connection is terminated
         if message_recv == '':
             print "Connection with client terminated"
             break
-        print handle + "> " + message_recv
+        #Prints out the message recieved from client
+        print clienthandle + "> " + message_recv
+        #Takes input from terminal, from 0 to 500 bytes
         while len(message_send) > 500 or len(message_send) == 0:
             message_send = raw_input(handle + "> ")
+        #If the message is "\quit", then terminate the connection
         if message_send == "\quit":
             print "Connection to client terminated"
             break
+        #Send the message
         sock.send(mssage_send)
         
 
 if __name__ == "__main__":
 
+    #Create a TCP socket with the Sockets API
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #Ensure that a port is given to the program and that its a valid number
     if (len(sys.argv) > 1):
         if (int(sys.argv[1]) > 0):
             port = int(sys.argv[1])
@@ -40,11 +51,14 @@ if __name__ == "__main__":
         valid_port_error()
 
     handle = ""
+    #Ask for handle from user, up to 10 characters
     while len(handle) > 10 or len(handle) == 0:
         handle = raw_input("Enter a handle: ")
+    #Bind socket to port and start listening for incomming connections
     s.bind(("", port))
     s.listen(1)
     print handle + ">"
+    #For any new connection, accept it, get the clienthandle, and start message loop until user closes program
     while 1:
         sock, address = s.accept()
         clienthandle = connection_to_client(sock, handle)
