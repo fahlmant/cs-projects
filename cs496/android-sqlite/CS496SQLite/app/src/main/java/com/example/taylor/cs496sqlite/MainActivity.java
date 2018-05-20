34b6f74eb6d809ebd9e083f8b0d15e57d675c2a3
fahlmant <fahlmantaylor@gmail.com>
2018-05-19 17:55:18 -0700

    mostly working

--- cs496/android-sqlite/CS496SQLite/app/src/main/java/com/example/taylor/cs496sqlite/MainActivity.java
+++ cs496/android-sqlite/CS496SQLite/app/src/main/java/com/example/taylor/cs496sqlite/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.taylor.cs496sqlite;
 
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabaseLockedException;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,6 +17,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -27,13 +35,17 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
     private LocationListener mLocationListener;
-    private int LOCATION_PERMISSIONS_REQUEST;
+    private int LOCATION_PERMISSIONS_REQUEST = 17;
 
     private SQLiteDatabase mDB;
 
     private TextView latitude;
     private TextView longitude;
-
+    private EditText et;
+    private SQLiteDatabase mSQLDB;
+    private Cursor mSQLCursor;
+    private CursorAdapter mSQLCursorAdapter;
+    private Boolean location_perms = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +56,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         final Button submit = findViewById(R.id.button);
         latitude = findViewById(R.id.latitude);
         longitude = findViewById(R.id.longitude);
+        et = findViewById(R.id.plain_text_input);
+
+        latitude.setText("44.5");
+        longitude.setText("-123.2");
 
         //Initialize Google Api Client
         if (mGoogleApiClient == null) {
@@ -57,35 +73,58 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         //Set up location request
         mLocationRequest = LocationRequest.create();
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(5000);
-        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setInterval(50);
+        mLocationRequest.setFastestInterval(50);
 
         //Set up location listener
         mLocationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
                 if (location != null) {
+                    Log.d("LOCATIONC", "Changed");
+                    latitude.setText(String.valueOf(location.getLatitude()));
+                    longitude.setText(String.valueOf(location.getLongitude()));
 
                 } else {
-
+                    latitude.setText("44.5");
+                    longitude.setText("-123.2");
                 }
             }
         };
 
         //Set up database
-        SQLiteHelper helper = new SQLiteHelper(this, "cs496.db", null, 1);
-
-
-
-        //Set up listview
+        SQLiteHelper helper = new SQLiteHelper(this, " ", null, 1);
+        mSQLDB = helper.getWritableDatabase();
 
         //Set up button listener to submit data to database
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Put string and location into database
+
+                String submitted_text = et.getText().toString();
+                if(submitted_text != "") {
+                    String lat = latitude.getText().toString();
+                    String longi = longitude.getText().toString();
+
+                    ContentValues submitted_data = new ContentValues();
+                    submitted_data.put(SQLiteHelper.DBContract.DataTable.COLUMN_NAME_DATA_STRING, submitted_text);
+                    if(location_perms) {
+                        submitted_data.put(SQLiteHelper.DBContract.DataTable.COLUMN_NAME_DATA_LAT, lat);
+                        submitted_data.put(SQLiteHelper.DBContract.DataTable.COLUMN_NAME_DATA_LONG, longi);
+                    }
+                    else {
+                        submitted_data.put(SQLiteHelper.DBContract.DataTable.COLUMN_NAME_DATA_LAT, "44.5");
+                        submitted_data.put(SQLiteHelper.DBContract.DataTable.COLUMN_NAME_DATA_LONG, "-123.2");
+                    }
+
+                    mSQLDB.insert(SQLiteHelper.DBContract.DataTable.TABLE_NAME,null,submitted_data);
+                    et.setText(null);
+                    populateTable();
+                }
             }
         });
+
+        populateTable();
     }
 
     @Override
@@ -109,15 +148,19 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             return;
         }
         LocationServices.FusedLocationApi.requestLocationUpdates(
-                mGoogleApiClient,mLocationRequest,mLocationListener);
+            mGoogleApiClient,mLocationRequest,mLocationListener);
+
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSIONS_REQUEST);
+            return;
         }
         Log.d("TEST", "onConnected");
+        updateLocation();
+
     }
 
     @Override
@@ -128,6 +171,50 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+
+        if(permissions[0].equals("android.permission.ACCESS_FINE_LOCATION") && grantResults[0] == 0) {
+            location_perms = true;
+        }
+        else {
+           location_perms = false;
+        }
+
+    }
+
+    private void populateTable() {
+        if(mSQLDB != null) {
+            try {
+                if(mSQLCursorAdapter != null && mSQLCursorAdapter.getCursor() != null) {
+                    if(!mSQLCursorAdapter.getCursor().isClosed()) {
+                        mSQLCursorAdapter.getCursor().close();
+                    }
+                }
+                mSQLCursor = mSQLDB.query(SQLiteHelper.DBContract.DataTable.TABLE_NAME,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null);
+                ListView SQListView = findViewById(R.id.sql_list);
+                mSQLCursorAdapter = new SimpleCursorAdapter(this,
+                        R.layout.sql_item,
+                        mSQLCursor,
+                        new String[]{SQLiteHelper.DBContract.DataTable.COLUMN_NAME_DATA_STRING,
+                                SQLiteHelper.DBContract.DataTable.COLUMN_NAME_DATA_LAT,
+                                SQLiteHelper.DBContract.DataTable.COLUMN_NAME_DATA_LONG},
+                        new int[]{R.id.sql_string_data, R.id.sql_int_lat, R.id.sql_int_long},
+                        0);
+                SQListView.setAdapter(mSQLCursorAdapter);
+            }
+            catch(Exception e) {
+                Log.d("POPULATE", "Error loading db " + e );
+            }
+        }
     }
 }
 
--- cs496/android-sqlite/CS496SQLite/app/src/main/java/com/example/taylor/cs496sqlite/SQLiteHelper.java
+++ cs496/android-sqlite/CS496SQLite/app/src/main/java/com/example/taylor/cs496sqlite/SQLiteHelper.java
@@ -3,22 +3,47 @@ package com.example.taylor.cs496sqlite;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+import static com.example.taylor.cs496sqlite.SQLiteHelper.DBContract.DataTable.SQL_CREATE_TABLE;
 
 public class SQLiteHelper extends SQLiteOpenHelper {
 
     public SQLiteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context, DBContract.DataTable.DB_NAME, factory, DBContract.DataTable.DB_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        final String BUILD_TABLE = "CREATE TABLE foo();";
 
-        db.execSQL(BUILD_TABLE);
+        db.execSQL(SQL_CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+    public class DBContract {
+        private DBContract(){};
+        public final class DataTable implements BaseColumns {
+
+            public static final String DB_NAME = "cs496";
+            public static final String TABLE_NAME = "data";
+            public static final String COLUMN_NAME_DATA_STRING = "data_string";
+            public static final String COLUMN_NAME_DATA_LAT = "data_lat";
+            public static final String COLUMN_NAME_DATA_LONG= "data_long";
+            public static final int DB_VERSION = 1;
+
+
+            public static final String SQL_CREATE_TABLE = "CREATE TABLE " +
+                    DataTable.TABLE_NAME + "(" + DataTable._ID + " INTEGER PRIMARY KEY NOT NULL," +
+                    DataTable.COLUMN_NAME_DATA_STRING + " VARCHAR(255)," +
+                    DataTable.COLUMN_NAME_DATA_LONG + " VARCHAR(255)," +
+                    DataTable.COLUMN_NAME_DATA_LAT + " VARCHAR(255));";
+
+            public  static final String SQL_DROP_DEMO_TABLE = "DROP TABLE IF EXISTS " + DataTable.TABLE_NAME;
+        }
+    }
+
 }
